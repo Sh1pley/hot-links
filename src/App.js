d98bbd447e7098f52d7aec4662dc56d1fbb838eb
Jesse Shipley <jesse.radack@gmail.com>
2017-04-17 21:32:25 -0600

    small break for dinner and kids, react still playing dirty

--- src/App.js
+++ src/App.js
@@ -1,18 +1,60 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase/app'
+import Link from './Link'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.setLinks = this.setLinks.bind(this)
+    this.state = {
+      isFetching: false
+    }
+  }
+
+  componentWillMount() {
+    this.getLinks();
+  }
+
+  getLinks = () => {
+    this.setState({ isFetching: true })
+    fetch('https://hotness.herokuapp.com/api/v1/hotreads')
+    .then( (response) => {
+      response.json().then((json) => {
+        this.setLinks(json)
+      })
+    })
+  }
+
+  setLinks = (links) => {
+    this.setState({ 
+      links: links,
+      isFetching: false
+    });
+  }
+
   render() {
+    let linksShow = null
+    if (this.state.isFetching === false) {
+      linksShow = this.state.links.map((link) => {
+        return {link}
+      })
+        
+    } else {
+      linksShow = <div> Loading... </div>
+    }
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="links-main">
+        <div className="header">
+          <div className='container'> 
+            <h2>Links!</h2>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className='container'>
+          <div className='links-list'> 
+            { linksShow }
+          </div>
+        </div>
       </div>
     );
   }
