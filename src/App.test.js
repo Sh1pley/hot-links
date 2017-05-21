4a4be0c416b695ce1e9d30aac4bbe4e0e157434b
Jesse Shipley <jesse.r.shipley@gmail.com>
2017-05-21 10:38:02 -0600

    Testing (#3)
    * update readme
    
    * enzyme is broked, but wrote some basic tests anyway

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {shallow, mount} from 'enzyme'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
+
+it('App renders a loading screen when waiting for links', () => {
+  const wrapper = shallow(<App />)
+
+  console.log(wrapper.find('.loading').type())
+  expect(wrapper.find('.loading').type()).to.toEqual('h1')
+})
+
+it('App has a container for links', () => {
+  const wrapper = shallow(<App />)
+
+  console.log(wrapper.find('.container h2').text())  
+  expect(wrapper.find('.container h2').text()).to.toEqual('Links!')
+})
